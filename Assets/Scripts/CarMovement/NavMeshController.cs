d944c439be39b7a5f64ebe411f3f2995d7bbcf63
MaartenBeute <19091@greijdanus.nl>
2021-11-29 20:02:08 +0100

    random destinations

--- Assets/Scripts/CarMovement/NavMeshController.cs
+++ Assets/Scripts/CarMovement/NavMeshController.cs
@@ -5,12 +5,27 @@ using UnityEngine.AI;
 
 public class NavMeshController : MonoBehaviour
 {
-    public Transform goal;
+    public Transform[] goal;
     public NavMeshAgent agent;
+    Transform target;
 
     void Start()
     {
         
-        agent.destination = goal.position;
     }
+
+    public void Update()
+    {
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    agent.destination = goal[Mathf.FloorToInt(Random.Range(0,94.9f))].position;
+                }
+            }
+        }
+    }
+    
 }

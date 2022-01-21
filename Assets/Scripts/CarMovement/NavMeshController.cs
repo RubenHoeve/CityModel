95b73f0145cad6197d5ddeb0e02aaf47891fbbf5
MaartenBeute <19091@greijdanus.nl>
2022-01-21 20:03:24 +0100

    laatste versie

--- Assets/Scripts/CarMovement/NavMeshController.cs
+++ Assets/Scripts/CarMovement/NavMeshController.cs
@@ -7,15 +7,18 @@ public class NavMeshController : MonoBehaviour
 {
     public Transform[] goal;
     public NavMeshAgent agent;
-    public float averageSpeed = 20;
+    public float averageSpeed = 14;
     public float averageSteering = 250;
-    public float averageAcceleration = 20;
-    public float RandomVMult = 5;
+    public float averageAcceleration = 3.5f;
+    public float averageDeceleration = 4.5f;
+    public float RandomVMult = 0.5f;
     public float RandomAMult = 0.5f;
+    public float RandomDMult = 0.5f;
     public bool AI;
     public int numTimes=12;
     float maxSpeed;
     float maxAcceleration;
+    float maxDeceleration;
     float speed;
     Transform target;
 
@@ -33,48 +36,65 @@ public class NavMeshController : MonoBehaviour
             maxSpeed = averageSpeed - ( r * averageSpeed * RandomVMult);
             agent.angularSpeed = averageSteering - (r * averageSteering * RandomVMult);
             maxAcceleration = averageAcceleration - (Random.Range(-1f, 1f) * averageAcceleration * RandomAMult);
+            maxDeceleration = averageDeceleration - (Random.Range(-1f, 1f) * averageDeceleration * RandomDMult);
         }
         else
         {
             maxSpeed = averageSpeed;
             maxAcceleration = averageAcceleration;
+            maxDeceleration = averageDeceleration;
         }
         agent.acceleration = maxAcceleration;
     }
 
-    public void Update()
+    public void FixedUpdate()
     {
 
-        time += Time.deltaTime;
+        time += Time.fixedDeltaTime;
         if (!agent.pathPending)
         {
             if (agent.remainingDistance <= agent.stoppingDistance)
             {
                 if (!agent.hasPath)
                 {
-                    agent.destination = goal[Mathf.FloorToInt(Random.Range(0, goal.Length))].position;
+                    newdest();
                     time = 0f;
                 }
-                else if(agent.velocity.sqrMagnitude == 0f)
+                else if(agent.velocity.sqrMagnitude <= 2f)
                 {
-                    agent.destination = goal[Mathf.FloorToInt(Random.Range(0,goal.Length))].position;
+                    newdest();
                     sendTime();
                     time = 0f;
                 }
             }
         }
-        if (agent.hasPath)
+        if (agent.hasPath&&agent.path.corners.Length>=2)
         {
             
             if (agent.isOnOffMeshLink)
             {
                 vel = (agent.currentOffMeshLinkData.endPos - transform.position);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(vel, Vector3.up), agent.angularSpeed * Time.deltaTime * 3f);
-                agent.Move(transform.forward*speed*Time.deltaTime);
-                speed += agent.acceleration * Time.deltaTime;
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(vel, Vector3.up), agent.angularSpeed * Time.fixedDeltaTime * 3f);
+                agent.Move(transform.forward*speed*Time.fixedDeltaTime);
+
+                float t = speed / maxDeceleration;
+                float stoppingD = 0.5f * maxDeceleration * t * t;
+                RaycastHit Hit;
+
+                bool hit = Physics.Raycast(transform.position + (transform.up * 0.5f), transform.forward, out Hit, stoppingD + 3);
+
+                if (!hit && agent.remainingDistance >= stoppingD)
+                {
+                    speed += maxAcceleration * Time.fixedDeltaTime;
+                }
+                else
+                {
+                    speed -= maxDeceleration * Time.fixedDeltaTime;
+                }
+
                 speed = Mathf.Clamp(speed, 0f, maxSpeed);
 
-                if ((agent.currentOffMeshLinkData.endPos - transform.position).magnitude < Time.deltaTime * maxSpeed)
+                if ((agent.currentOffMeshLinkData.endPos - transform.position).magnitude <= Time.fixedDeltaTime * maxSpeed * 1.5f)
                 {
                     agent.CompleteOffMeshLink();
                 }
@@ -82,18 +102,23 @@ public class NavMeshController : MonoBehaviour
             else
             {
                 vel = (agent.path.corners[1] - transform.position);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(vel, Vector3.up), agent.angularSpeed * Time.deltaTime);
-                agent.Move(transform.forward * speed * Time.deltaTime);
-                float t = speed / agent.acceleration;
-                float stoppingD = 0.5f * agent.acceleration * t * t;
-                RaycastHit hit;
-                if (!Physics.Raycast(transform.position + (transform.forward * 5) + transform.up, transform.forward, out hit, stoppingD + 1) && agent.remainingDistance >= stoppingD)
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(vel, Vector3.up), agent.angularSpeed * Time.fixedDeltaTime);
+                agent.Move(transform.forward * speed * Time.fixedDeltaTime);
+                float t = speed / maxDeceleration;
+                float stoppingD = 0.5f * maxDeceleration * t * t;
+                RaycastHit Rhit;
+                RaycastHit Lhit;
+
+                bool rhit = Physics.Raycast(transform.position + (transform.up * 0.5f) + (transform.right * 0.8f), transform.forward, out Rhit, stoppingD + 3);
+                bool lhit = Physics.Raycast(transform.position + (transform.up * 0.5f) + (transform.right * -0.8f), transform.forward, out Lhit, stoppingD + 3);
+
+                if (!rhit && !lhit && agent.remainingDistance >= stoppingD)
                 {
-                    speed += agent.acceleration * Time.deltaTime;
+                    speed += maxAcceleration * Time.fixedDeltaTime;
                 }
                 else
                 {
-                    speed -= agent.acceleration * Time.deltaTime;
+                    speed -= maxDeceleration * Time.fixedDeltaTime;
                 }
 
                 speed = Mathf.Clamp(speed, 0f, maxSpeed);
@@ -114,6 +139,18 @@ public class NavMeshController : MonoBehaviour
             numTimes--;
         }
         
+    }
+
+    private void newdest()
+    {
+        bool found = true;
+        Vector3 dest = Vector3.zero;
+        while (found)
+        {
+            dest = goal[Mathf.FloorToInt(Random.Range(0, goal.Length))].position;
+            found = (transform.position - dest).magnitude < 75;
+        }
+        agent.destination = dest;
     }
 
     public void OnCollisionEnter(Collision collision)
